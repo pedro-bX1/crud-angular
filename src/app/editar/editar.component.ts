f09516f9ae340f33e56e678d7558da617856eb8e
Pedro Bruno <55460469+pedro-bX1@users.noreply.github.com>
2022-05-26 14:53:01 -0300

    adicionar value em especializacao

--- src/app/editar/editar.component.ts
+++ src/app/editar/editar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from "@angular/forms";   
+import { FormControl } from "@angular/forms";   
 import { Pessoas } from '../_interfaces/pessoas.interface';
+import { PessoasService } from '../_services/pessoas.service';
 
 @Component({
   selector: 'app-editar',
@@ -21,7 +22,7 @@ export class EditarComponent implements OnInit {
   ngOnInit(): void {
   }
   editar(){
-    this.servicoPessoas.guardarPessoas(this.nome.value, this.matricula.value, this.salario.value, this.cargo.value, this.especializacao).subscribe((pessoas:Pessoas[])=>{
+    this.servicoPessoas.guardarPessoas(this.nome.value, this.matricula.value, this.salario.value, this.cargo.value, this.especializacao.value).subscribe((pessoas:any)=>{
       console.log(snap);
     });
     this.ngOnInit();
