22a991079a186216b53153b92214f4e7fd4c8838
Pedro Bruno <55460469+pedro-bX1@users.noreply.github.com>
2022-05-26 13:20:00 -0300

    adição da função editar pessoas

--- src/app/_services/pessoas.service.ts
+++ src/app/_services/pessoas.service.ts
@@ -8,21 +8,28 @@ import { Pessoas } from '../_interfaces/pessoas.interface';
 export class PessoasService {
   id!:string;
   constructor(private http: HttpClient) { }
+
   obterPessoas(){
   return this.http.get('http://localhost/phpCrud/consulta.php');
 }
+
   guardarPessoas(nome:string, matricula:number, salario:number, cargo:string,especializacao:string){
-    return this.http.get('http://localhost/phpCrud/salvar.php?nome='%nome%'&matricula='%matricula%'&salario='%salario%'&cargo='%cargo%'&especializacao='%espacializacao%'');
+    return this.http.get('http://localhost/phpCrud/salvar.php?nome='+nome+'&matricula='+matricula+'&salario='+salario+'&cargo='+cargo+'&especializacao='+especializacao+'');
   }
 
   apagarPessoas(id:string){
-    return this.http.get('http://localhost/phpCrud/apagar.php?id='id);
-  }setId(id:string){
+    return this.http.get('http://localhost/phpCrud/apagar.php?id='+id);
+  }
+  
+  setId(id:string){
     this.id=id;
-  }getId(){
+  }
+  
+  getId(){
     this.id;
   }
+  
   editarPessoas(nome:string, matricula:number, salario:number, cargo:string,especializacao:string){
-    return this.http.get('http://localhost/phpCrud/editar.php?nome='%nome%'&matricula='%matricula%'&salario='%salario%'&cargo='%cargo%'&especializacao='%espacializacao%'');
+    return this.http.get('http://localhost/phpCrud/editar.php?nome='+nome+'&matricula='+matricula+'&salario='+salario+'&cargo='+cargo+'&especializacao='+especializacao+'&id'+this.id);
   }
 }
